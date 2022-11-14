08ea7a613998077f40ea8943c21c5d03462d3350
Amit Sandhel <amit.sandhel@gmail.com>
2022-11-14 15:45:30 -0500

    Direction Model and Service Added (#6)
    * Direction Model and Service Added
    Added packages for postgresql and mongodb
    Added direction table as a model and service as generated by the EFCore
    
    * Added xml code to the Direction class and added an attribute name for abbr
    
    * added comments and a copyright header
    
    * added copyright header and fixed the xml code a bit more
    
    * fixed the xml code and renamed context file
    
    * reversed the name of abbr to abbreviation
    
    * explained why we need the Directions

---
+++ CCDRS/Data/CCDRSContext.cs
@@ -0,0 +1,57 @@
+﻿/*
+    Copyright 2022 University of Toronto
+    This file is part of CCDRS.
+    CCDRS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    CCDRS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with CCDRS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using CCDRS.Model;
+using Microsoft.EntityFrameworkCore;
+namespace CCDRS.Data;
+
+public partial class CCDRSContext : DbContext
+{
+    public CCDRSContext()
+    {
+    }
+
+    public CCDRSContext(DbContextOptions<CCDRSContext> options)
+        : base(options)
+    {
+    }
+
+    /// <summary>
+    /// Allow pages to access the Directions Class as a service
+    /// </summary>
+    public virtual DbSet<Direction> Directions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Direction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("direction_pkey");
+
+            entity.ToTable("direction");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Abbr)
+                .HasMaxLength(1)
+                .HasColumnName("abbr");
+            entity.Property(e => e.Compass).HasColumnName("compass");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ CCDRS/Model/Direction.cs
@@ -0,0 +1,43 @@
+﻿/*
+    Copyright 2022 University of Toronto
+    This file is part of CCDRS.
+    CCDRS is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    CCDRS is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with CCDRS.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CCDRS.Model;
+/// <summary>
+/// Class that maps to the PostgreSql direction table
+/// </summary>
+public partial class Direction
+{
+    /// <summary>
+    /// Id is a Primary serial key of type int
+    /// It is auto generated. 
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Compass string generated of the name of directions eg North, South
+    /// </summary>
+    public string Compass { get; set; } = String.Empty;
+
+    /// <summary>
+    /// Abbreviation string generated of the abbreviated form of each direction
+    /// Changed the attribute name from Abbr to Abbreviation
+    /// </summary>
+    [Column("Abbr")]
+    public char Abbreviation { get; set; }
+}
